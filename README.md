637705e804af6946bc826dddc0f161341ba4f8ae
BonHyuck <qhsgur0126@gmail.com>
2021-08-19 15:08:15 +0900

    210819 나중에 이어서 다 풀기

--- README.md
+++ README.md
@@ -287,10 +287,13 @@ https://www.acmicpc.net/problem/1132
 
 .5502. [G3] 팰린드롬, https://www.acmicpc.net/problem/5502
 
+#### 2021.08.19(목)
+
+.14630. [G3] 변신로봇, https://www.acmicpc.net/problem/14630
+
 ```
 
-14630. [G3] 변신로봇
-https://www.acmicpc.net/problem/14630
+
 1766. [G2] 문제집
 https://www.acmicpc.net/problem/1766
 10423. [G2] 전기가 부족해
---
+++ codes/14630.py
@@ -0,0 +1,44 @@
+'''
+3
+11
+33
+55
+1 3
+'''
+
+def find_path(idx, dist):
+    global result
+    if dist > result:
+        return
+
+    if idx == end:
+        if dist < result:
+            result = dist
+        return
+
+    
+
+
+N = int(input())
+parts = list(input() for _ in range(N))
+money = [[float('inf') for _ in range(N)] for _ in range(N)]
+for start in range(N):
+    for end in range(N):
+        total = 0
+        if start != end:
+            for k in range(len(parts[start])):
+                total += int((int(parts[start][k]) - int(parts[end][k])) ** 2)
+            money[start][end] = total
+            money[end][start] = total
+
+start, end = map(int, input().split())
+start -= 1
+end -= 1
+result = float('inf')
+
+visited = [[0 for _ in range(N)] for _ in range(N)]
+
+find_path(start, 0)
+
+print(money)
+print(parts)
